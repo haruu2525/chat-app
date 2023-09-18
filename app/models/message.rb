448b46496a2ca4f68df97b8815afaf37e0818c1e
haruu2525 <tech.haru551@gmail.com>
2023-09-18 20:22:44 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-
-  validates :content, presence: true
+  has_one_attached :image
   
+  validates :content, presence: true
+
 end
