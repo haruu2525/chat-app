0b9ba53ef9c54342ec4e66a44aa0a7df5f7c3cc5
haruu2525 <tech.haru551@gmail.com>
2023-09-19 17:42:35 +0900

    結合テストの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,8 +10,8 @@ class MessagesController < ApplicationController
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
-    else@messages = @room.messages.includes(:user)
-
+    else
+      @messages = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
     end
   end
